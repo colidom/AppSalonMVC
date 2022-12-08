b9da64526ba0867b5d39b8db18ca88ef6a75f8bf
colidom <colidom@outlook.com>
2022-12-08 21:52:39 +0000

    Creando el Formulario de Autenticación

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,21 @@
 <h1 class="nombre-pagina">Login</h1>
 
 <p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<form class="formulario" action="/" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" id="email" placeholder="Tu email" name="email">
+    </div>
+    <div class="campo">
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" placeholder="Tu password" name="password">
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar sesión">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">Crear cuenta</a>
+    <a href="/olvide">Recuperar contraseña</a>
+</div>
