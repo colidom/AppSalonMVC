80224a571141dec0c71b3fde60fe35b61b8c454e
colidom <colidom@outlook.com>
2022-12-10 03:28:04 +0000

    Instanciando y sincronizando Usuario con el Formulario

--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -4,27 +4,27 @@
 <form class="formulario" method="POST" action="/crear-cuenta">
     <div class="campo">
         <label for="nombre">Nombre</label>
-        <input type="text" id="nombre" name="nombre" placeholder="Tu nombre">
+        <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" value="<?php echo s($usuario->nombre) ?>">
     </div>
 
     <div class="campo">
         <label for="apellido">Apellido</label>
-        <input type="text" id="apellido" name="apellido" placeholder="Tu Apellido">
+        <input type="text" id="apellido" name="apellido" placeholder="Tu Apellido" value="<?php echo s($usuario->apellido) ?>">
     </div>
 
     <div class="campo">
         <label for="telefono">Teléfono</label>
-        <input type="tel" id="telefono" name="telefono" placeholder="Tu Teléfono">
+        <input type="tel" id="telefono" name="telefono" placeholder="Tu Teléfono" value="<?php echo s($usuario->telefono) ?>">
     </div>
 
     <div class="campo">
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="Tu Email">
+        <input type="email" id="email" name="email" placeholder="Tu Email" value="<?php echo s($usuario->email) ?>">
     </div>
 
     <div class="campo">
         <label for="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Tu Password">
+        <input type="password" id="password" name="password" placeholder="Tu Password" value="<?php echo s($usuario->password) ?>">
     </div>
 
     <input type="submit" value="Crear cuenta" class="boton">
