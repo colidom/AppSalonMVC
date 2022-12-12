9d1f52f6556e5d7ce3bc437323cafeb1f03a6e4b
colidom <colidom@outlook.com>
2022-12-12 01:21:40 +0000

    Añadida db a la conexión con la misma

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect('localhost', 'colidom', 'Mysql2021', '');
+$db = mysqli_connect('localhost', 'colidom', 'Mysql2021', 'appsalon_mvc');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
