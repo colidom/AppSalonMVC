9ddf371513d09d66bd6e1e9ac992e3beafe084b8
colidom <colidom@outlook.com>
2022-12-15 01:44:44 +0000

    Cambiados valores por defecto de admin y confirmado

--- models/Usuario.php
+++ models/Usuario.php
@@ -28,8 +28,8 @@ class Usuario extends ActiveRecord
         $this->telefono = $args['telefono'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
-        $this->admin = $args['admin'] ?? null;
-        $this->confirmado = $args['confirmado'] ?? null;
+        $this->admin = $args['admin'] ?? 0;
+        $this->confirmado = $args['confirmado'] ?? 0;
         $this->token = $args['token'] ?? '';
     }
 
