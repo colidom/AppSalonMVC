0169489d1b0f8412032c7f0717f3e3ed19c85fd2
colidom <colidom@outlook.com>
2022-12-19 23:58:56 +0000

    Mostrar el Nombre del usuario autenticado

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -8,6 +8,8 @@ class CitaController
 {
     public static function index(Router $router)
     {
-        $router->render('cita/index', []);
+        $router->render('cita/index', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 }
--- views/cita/index.php
+++ views/cita/index.php
@@ -4,15 +4,29 @@
 <div id="app">
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
-        <p>Elige tus servicios a continuación</p>
+        <p class="text-center">Elige tus servicios a continuación</p>
         <div id="servicios" class="listado-servicios"></div>
     </div>
     <div id="paso-2" class="seccion">
         <h2>Tus datos y cita</h2>
-        <p>Datos y fecha de cita</p>
+        <p class="text-center">Datos y fecha de la cita</p>
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input id="nombre" type="text" placeholder="Tu nombre" value="<?php echo $nombre ?>" disabled />
+            </div>
+            <div class="campo">
+                <label for="fecha">Fecha</label>
+                <input id="fecha" type="date" />
+            </div>
+            <div class="campo">
+                <label for="hora">Hora</label>
+                <input id="hora" type="time" />
+            </div>
+        </form>
     </div>
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
-        <p>Verifica que la información sea correcta</p>
+        <p class="text-center">Verifica que la información sea correcta</p>
     </div>
 </div>
