71a9872a58e2c36d1985b12f00de8197fef59634
colidom <colidom@outlook.com>
2022-12-22 23:54:28 +0000

    Mostrando los Servicios de nuestra API

--- src/js/app.js
+++ src/js/app.js
@@ -98,3 +98,26 @@ async function consultarAPI() {
     console.log(error);
   }
 }
+
+function mostrarServicios(servicios) {
+  servicios.forEach((servicio) => {
+    const { id, nombre, precio } = servicio;
+
+    const nombreServicio = document.createElement("P");
+    nombreServicio.classList.add("nombre-servicio");
+    nombreServicio.textContent = nombre;
+
+    const precioServicio = document.createElement("P");
+    precioServicio.classList.add("precio-servicio");
+    precioServicio.textContent = `${precio}€`;
+
+    const servicioDiv = document.createElement("DIV");
+    servicioDiv.classList.add("servicio");
+    servicioDiv.dataset.idServicio = id;
+
+    servicioDiv.appendChild(nombreServicio);
+    servicioDiv.appendChild(precioServicio);
+
+    document.querySelector("#servicios").appendChild(servicioDiv);
+  });
+}
