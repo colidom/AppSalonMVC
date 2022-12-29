c083fa953f6031e129f385124e25f27b8094ed1f
colidom <colidom@outlook.com>
2022-12-29 01:43:34 +0000

    Proteger la Ruta de Citas

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -8,6 +8,10 @@ class CitaController
 {
     public static function index(Router $router)
     {
+
+        // Comprobamos si el usuario está utenticado o no
+        isAuth();
+
         $router->render('cita/index', [
             'id' => $_SESSION['id'],
             'nombre' => $_SESSION['nombre']
