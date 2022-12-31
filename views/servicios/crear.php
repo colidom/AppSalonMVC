77f0bf8d467dce6e6cacdb8ac3c94d6b952bd855
colidom <colidom@outlook.com>
2022-12-31 21:13:49 +0000

    Añadida la barra para manejar servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -3,7 +3,7 @@
 <p class="descripcion-pagina">Añade un nuevo servicio</p>
 
 <?php
-/* include_once  __DIR__ . '/../templates/barra.php'; */
+include_once  __DIR__ . '/../templates/barra.php';
 include_once  __DIR__ . '/../templates/alertas.php';
 ?>
 
