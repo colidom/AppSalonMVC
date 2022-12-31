b63161ddde4cdd48f33c0c8d3ebdce443f5d1d55
colidom <colidom@outlook.com>
2022-12-31 03:03:16 +0000

    Eliminar Citas

--- views/admin/index.php
+++ views/admin/index.php
@@ -48,6 +48,11 @@ if (count($citas) === 0) {
                 $proximo = $citas[$key + 1]->id ?? 0;
                 if (esUltimo($actual, $proximo)) { ?>
                     <p class="total">Total: <span><?php echo $total . "€"; ?></span></p>
+                    <form action="/api/eliminar" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $cita->id; ?>">
+                        <input type="submit" class="boton-eliminar" value="Eliminar">
+                    </form>
+
             <?php }
             } // Fin de ForEach 
             ?>
