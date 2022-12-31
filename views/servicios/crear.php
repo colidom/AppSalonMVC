d7c716d189b53f8e361a3ce3d75a37af4603d52c
colidom <colidom@outlook.com>
2022-12-31 20:56:27 +0000

    Creando el Formulario para Servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -5,3 +5,9 @@
 <?php
 include_once  __DIR__ . '/../templates/barra.php';
 ?>
+
+<form action="/servicios/crear" method="POST" class="formulario">
+    <?php include_once __DIR__ . '/formulario.php'; ?>
+
+    <input type="submit" class="boton" value="Crear servicio">
+</form>
