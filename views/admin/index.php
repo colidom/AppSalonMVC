36b452b00cc2238696c18a47a77650971b502c6d
colidom <colidom@outlook.com>
2022-12-31 01:48:09 +0000

    Añadida clase al formulario de fecha en buscar citas

--- views/admin/index.php
+++ views/admin/index.php
@@ -7,8 +7,10 @@ include_once __DIR__ . '/../templates/barra.php'
 <h2>Buscar Citas</h2>
 <div class="busqueda">
     <form class="formulario">
-        <label for="fecha">Fecha</label>
-        <input type="date" id="fecha" name="fecha">
+        <div class="campo">
+            <label for="fecha">Fecha</label>
+            <input type="date" id="fecha" name="fecha" />
+        </div>
     </form>
 </div>
 <div id="citas-admin">
