3fc01aae13cc374032021c4c5dc3795938632c25
colidom <colidom@outlook.com>
2022-12-31 13:05:59 +0000

    Creando las Vistas para los Servicios

---
+++ views/servicios/actualizar.php
@@ -0,0 +1,7 @@
+<h1 class="nombre-pagina">Actualiar servicio</h1>
+
+<p class="descripcion-pagina">Modifica los valores del formulario</p>
+
+<?php
+include_once  __DIR__ . '/../templates/barra.php';
+?>
---
+++ views/servicios/crear.php
@@ -0,0 +1,7 @@
+<h1 class="nombre-pagina">Nuevo servicio</h1>
+
+<p class="descripcion-pagina">Añade un nuevo servicio</p>
+
+<?php
+include_once  __DIR__ . '/../templates/barra.php';
+?>
