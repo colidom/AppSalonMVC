358b4a51cf453f145cdbceb3f56abc1770bcf9e3
colidom <colidom@outlook.com>
2023-01-01 19:08:01 +0000

    Quitado salto de línea

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -9,6 +9,5 @@ include_once  __DIR__ . '/../templates/alertas.php';
 
 <form action="/servicios/crear" method="POST" class="formulario">
     <?php include_once __DIR__ . '/formulario.php'; ?>
-
     <input type="submit" class="boton" value="Crear servicio">
 </form>
