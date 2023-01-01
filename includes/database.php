4449449bfd4ea8e9c208d1a4985f528eca2d3ba8
colidom <colidom@outlook.com>
2023-01-01 20:06:45 +0000

    Crear Variables de Entorno

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,13 @@
 <?php
 
-$db = mysqli_connect('localhost', 'colidom', 'Mysql2021', 'appsalon_mvc');
+$db = mysqli_connect(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_BD']
+);
+
+/* debuguear($_ENV); */
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
